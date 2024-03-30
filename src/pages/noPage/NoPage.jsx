1c0cbd7ae972b61e8f25023d67df8adad0b34411
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2024-03-30 11:36:52 +0530

    Add NotFound Page

--- src/pages/noPage/NoPage.jsx
+++ src/pages/noPage/NoPage.jsx
@@ -1,9 +1,23 @@
+// import imagenot from "../../../public/img/notfound.png";
+import React from "react";
+
 const NoPage = () => {
-    return (
-        <div>
-            No Page
-        </div>
-    );
-}
+  return (
+    <div class="min-h-screen flex flex-grow items-center justify-center bg-gray-50">
+      <div class="rounded-lg bg-white p-8 text-center shadow-xl">
+        <h1 class="mb-4 text-4xl font-bold">404</h1>
+        <p class="text-gray-600">
+          Oops! The page you are looking for could not be found.
+        </p>
+        <a
+          href="/"
+          className="mt-4 inline-block rounded bg-pink-500 px-4 py-2 font-semibold text-white hover:bg-pink-200"
+        >
+          Go back to Home
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default NoPage;
